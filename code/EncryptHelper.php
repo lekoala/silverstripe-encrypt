e46461994e526ae7ca2f5e0d0858dd2270a497c9
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-04-27 11:50:24 +0700

    ENHANCEMENT: Make key path configurable

--- code/EncryptHelper.php
+++ code/EncryptHelper.php
@@ -1,6 +1,7 @@
 <?php
 namespace LeKoala\SilverStripeEncrypt;
 
+use League\Flysystem\Config;
 use Tuupola\Base62Proxy;
 use SilverStripe\Control\Director;
 
@@ -24,7 +25,10 @@ class EncryptHelper
         }
 
         // Generate the key for this server
-        $keyPath = Director::baseFolder() . '/secret.key';
+        $relativePath = Config::inst()->get('LeKoala\SilverStripeEncrypt\EncryptHelper', 'secret_key_path');
+        $keyPath = Director::baseFolder() . $relativePath;
+        echo 'KEYPATH: ' . $keyPath;
+        
         if (is_file($keyPath)) {
             $raw = file_get_contents($keyPath);
             $key = Base62Proxy::decode($raw);
